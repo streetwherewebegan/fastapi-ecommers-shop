5655bbd0d56c7e41119fb56b9b7884884a3fc2b4
streetwherewebegan <streetwherewebegan@gmail.com>
2026-07-02 16:45:10 -0400

    changes

--- app/database.py
+++ app/database.py
@@ -1,11 +1,23 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = 'sqlite:///ecommerce.db'
 
 engine = create_engine(DATABASE_URL, echo=True)
 
 SessionLocal = sessionmaker(bind=engine)
+
+# --------------- Асинхронное подключение к PostgreSQL -------------------------
+
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase
+
+DATABASE_URL = 'postgresql+asyncpg://ecommerce_user:admin@localhost:5432/ecommerce_db'
+
+async_engine = create_async_engine(DATABASE_URL, echo=True)
+
+async_session_maker = async_sessionmaker(async_engine, expire_on_commit=False, class_=AsyncSession)
+
 
 class Base(DeclarativeBase):
     pass
