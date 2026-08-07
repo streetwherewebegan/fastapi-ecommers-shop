4d68658509b0a3b216671139741f768c18609ba5
streetwherewebegan <streetwherewebegan@gmail.com>
2026-08-07 16:56:04 -0400

    added logger

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
+from loguru import logger
+from uuid import uuid4
+
 from app.routers import (
     categories, 
     cart, 
@@ -9,11 +13,32 @@ from app.routers import (
     users,
     )
 
+logger.add('info.log', format='"Log: [{extra[log_id]}:{time} - {level} - {message}]', level='INFO', enqueue=True)
 
 app = FastAPI(
     title='FastAPI Интернет-магазин',
     version='0.1.0',
 )
+
+
+@app.middleware('http')
+async def log_middleware(request: Request, call_next):
+    log_id = str(uuid4())
+    with logger.contextualize(log_id=log_id):
+        try:
+            response = await call_next(request)
+            if response.status_code in [401, 402, 403, 404]:
+                logger.warning('Request to {request.url.path} failed')
+            else:
+                logger.info('Successfully accessed ' + request.url.path)
+        except Exception as ex:
+            logger.error(f'Request to {request.url.path} failed: {ex}')
+            response = JSONResponse(
+                content={'success': False}, 
+                status_code=500,
+            )
+        return response
+
 
 app.mount('/media', StaticFiles(directory='media'), name='media')
 
